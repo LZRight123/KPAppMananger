dd2f72c80cc363534ed32545c6af90939b77fd7a
快乐的小梁同学 <350442340@qq.com>
2021-08-13 20:39:49 +0800

    first commit

--- script/start.sh
+++
@@ -1,8 +0,0 @@
-
-TARGET_NAME="Example"
-
-
-ln -fhs "${BUILT_PRODUCTS_DIR}" "${PROJECT_DIR}"/LatestBuild
-cp ${PROJECT_DIR}/../script/* "${BUILT_PRODUCTS_DIR}"
-ldid -Sad.entitlements ./${TARGET_NAME}.app/${TARGET_NAME}
-ideviceinstaller -i ./${TARGET_NAME}.app
